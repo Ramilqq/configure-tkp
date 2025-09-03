3f1e1031745eeb997be32c4aa76b6f32651fccce
Ramilqq_lab <jazzduem@yandex.ru>
2025-09-03 14:33:27 +0300

    v1.0.1

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -18,32 +18,28 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{route('home')}}">Главная</a>
+                            <a class="nav-link active" aria-current="page" href="{{route('home')}}">Главная</a>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="{{route('table-settings.template-list')}}">Шаблон</a>
+                            <a class="nav-link" href="{{route('table-settings.template-list')}}">Шаблон</a>
                         </li>
-                        <li class="nav-item dropdown">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('configuration-node-group')}}">Конфигуратор настройка</a>
+                        </li>
+                        <!--li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Страницы
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="{{route('table-settings.template-list')}}">Шаблон</a></li>
-                                <li><a class="dropdown-item" href="{{route('configuration')}}">Конфигуратор</a></li>
-                                <li><a class="dropdown-item" href="{{route('configuration-node-group')}}">Конфигуратор настройка</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
                                 <li><hr class="dropdown-divider"></li>
                                 <li><a class="dropdown-item" href="#">Something else here</a></li>
                             </ul>
-                        </li>
+                        </li-->
 
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                ТКП
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="{{route('tkp.contact')}}">Контактная информация</a></li>
-                                <li><a class="dropdown-item" href="{{route('tkp.contact.edit', ['id' => '1', 'tkp_version' => 1753691078])}}">Контактная информация 123</a></li>
-                            </ul>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('tkp.contact')}}">ТКП создать</a>
                         </li>
 
                         <li class="nav-item">
